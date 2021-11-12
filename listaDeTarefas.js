017f79aa6a6f57c2f71b0ed317a73b896c1ea6d5
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-12 15:52:08 -0300

    feat(form): Change list item by input data

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,14 @@
  const criarTarefa = (evento) => {
+
      evento.preventDefault()
+
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log (valor)
+
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content">${valor}</p>`
+    
+    tarefa.innerHTML = conteudo
     input.value = ""
 }
 
