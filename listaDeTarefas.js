c0f477c4771a63e8aa74057ec8368147e0602301
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-16 16:22:30 -0300

    feat(list): Add element to list

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,23 +1,20 @@
 // Define a função criar tarefa:
 const criarTarefa = (evento) => {
-    // Apaga eventos prévios do parâmetro passado para a função:
-     evento.preventDefault()
-    // Procura o elemento correspondente ao input no formulário:
-    const input = document.querySelector('[data-form-input]')
-    // Guarda o conteúdo/valor do input dentro da constante "valor"
-    const valor = input.value
-    // Procura o elemento da lista usando seletor data atributs:
-    const tarefa = document.querySelector('[data-task]')
-    // Transforma o valor recebido no input em html:
-    const conteudo = `<p class="content">${valor}</p>`
-    // substitui o elemento da lista guardado na constante "tarefa" pelo conteúdo recebido no input
-    tarefa.innerHTML = conteudo
-    // zera o valor do input:
-    input.value = ""
+    evento.preventDefault()                                         // Apaga eventos prévios do parâmetro passado para a função:
+    
+    const lista = document.querySelector('[data-list]')             // Procura o elemento que contém a lista:
+    const input = document.querySelector('[data-form-input]')       // Procura o elemento correspondente ao input no formulário:
+    const valor = input.value                                       // Guarda o conteúdo/valor do input dentro da constante "valor"
+
+    const tarefa = document.createElement('li')                     // Cria um elemento do tipo li
+    tarefa.classList.add('task')                                    // Acessa a lista de classes e adiciona uma classe
+    const conteudo = `<p class="content">${valor}</p>`              // Transforma o valor recebido no input em html:
+    
+    tarefa.innerHTML = conteudo                                     // acessa o conteúdo da tarefa e troca pelo recebido no input
+    lista.appendChild(tarefa)                                       // adiciona o conteúdo da tarefa na lista
+    input.value = ""                                                // zera o valor do input:
 }
 
-//define constante que procura o elemento com seletor "data-atributs" correspondente ao botão:
-const novaTarefa = document.querySelector('[data-form-button]')
 
-//adiciona um escutador de eventos ao botão anterior e, se o evento click acontecer, chama a função "criar tarefa"
-novaTarefa.addEventListener('click', criarTarefa)
+const novaTarefa = document.querySelector('[data-form-button]')    //define constante que procura o elemento com seletor "data-atributs" correspondente ao botão
+novaTarefa.addEventListener('click', criarTarefa)                  //adiciona um escutador de eventos ao botão anterior e, se o evento click acontecer, chama a função "criar tarefa"
