a75760e7f5f63ff25cf81c051a8fd0bd85586cac
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-16 15:52:21 -0300

    feat(README): Add project description

---
+++ README.md
@@ -0,0 +1,12 @@
+# Curso: JS na Web: Manipule o DOM com JavaScript
+
+## Formação Alura Front End
+---
+### Conteúdo
+- Como construir uma página dinâmica
+- Seletores "data-atributs"
+- Percorrendo a árvore do DOM (`document.querySelector()`)
+- Métodos para criar um elemento (`document.createElement()`)
+- Método para anexar um elemento dentro de outro (`.appendChild()`)
+- Método innerHTML
+- Método para acessar e adicionar uma classe a um elemento (`.classList.Add()`) 
--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,18 +1,23 @@
- const criarTarefa = (evento) => {
-
+// Define a função criar tarefa:
+const criarTarefa = (evento) => {
+    // Apaga eventos prévios do parâmetro passado para a função:
      evento.preventDefault()
-
+    // Procura o elemento correspondente ao input no formulário:
     const input = document.querySelector('[data-form-input]')
+    // Guarda o conteúdo/valor do input dentro da constante "valor"
     const valor = input.value
-
+    // Procura o elemento da lista usando seletor data atributs:
     const tarefa = document.querySelector('[data-task]')
+    // Transforma o valor recebido no input em html:
     const conteudo = `<p class="content">${valor}</p>`
-    
+    // substitui o elemento da lista guardado na constante "tarefa" pelo conteúdo recebido no input
     tarefa.innerHTML = conteudo
+    // zera o valor do input:
     input.value = ""
 }
 
-
+//define constante que procura o elemento com seletor "data-atributs" correspondente ao botão:
 const novaTarefa = document.querySelector('[data-form-button]')
 
+//adiciona um escutador de eventos ao botão anterior e, se o evento click acontecer, chama a função "criar tarefa"
 novaTarefa.addEventListener('click', criarTarefa)
