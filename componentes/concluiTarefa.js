23ec83799f8e52289e84ae80c3a081d873afbad7
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-11-22 11:42:11 -0300

    feat(complete-task): Stores completed state of task in local storage

--- componentes/concluiTarefa.js
+++ componentes/concluiTarefa.js
@@ -1,15 +1,19 @@
-const BotaoConclui = () => {
+const concluirTarefa = (atualiza, id) => {
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefasCadastradas[id].concluida = !tarefasCadastradas[id].concluida
+    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))
+
+    atualiza()
+} 
+
+const BotaoConclui = (atualiza, id) => {
     const botaoConclui = document.createElement('button')          //define um componente que cria um elemento botão
     botaoConclui.classList.add('check-button')
     botaoConclui.innerText = 'concluir'
-    botaoConclui.addEventListener('click', concluirTarefa)         //se o botão for clicado chama a função concluir tarefa         
+    botaoConclui.addEventListener('click', () => concluirTarefa(atualiza,id))         //se o botão for clicado chama a função concluir tarefa         
     return botaoConclui
 }
 
-const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target                             // seleciona o alvo do evento
-    const tarefaCompleta = botaoConclui.parentElement              // seleciona o pai do elemento que foi clicado
-    tarefaCompleta.classList.toggle('done')                        // acessa a lista de classes e o método toggle coloca ou retira a classe "done"
-}
 
 export default BotaoConclui
